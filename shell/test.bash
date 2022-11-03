ab30520c6fda06eabd673963bfb216e8e974aba8
matsuyamayusaku <s21c1114if@s.chibakoudai.jp>
2022-11-03 15:45:35 +0900

    adjust test

--- shell/test.bash
+++ shell/test.bash
@@ -3,28 +3,24 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-      echo NG at Line $1
-      res=1
+	echo ${1}行目が違うよ
+			res=1
 }
 
 res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 14 ] || ng ${LINENO}
-
-### I/O ###
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
+[ "${out}" = 15.0 ] || ng "${LINENO}"
 
 ### STRANGE INPUT ###
 out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+[ "$?" = 1 ] || ng "${LINENO}"
+[ "${out}" = "" ] || ng "${LINENO}"
 
-out=$(echo | ./plus) #空文字
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng "${LINENO}"
+[ "${out}" = "" ] || ng "${LINENO}"
 
-[ "$res" = 0 ] && echo OK
+[ "${res}" = 0 ] && echo OK
 exit $res
